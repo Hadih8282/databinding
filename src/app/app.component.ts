64a3a7a25771efc4dcfdd00e66c7ccaf10acb69b
Hadih8282 <hadi_h82@gmail.com>
2022-05-08 03:20:19 +0430

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'databinding';
+  name: string = "Hadi_h82";
+  // isEnabled: boolean = true;
+
+  public onInputChanged(e: Event) {
+    // console.log(e);
+
+    // this.name = (e.target as HTMLInputElement).value;
+    this.name = (<HTMLInputElement>e.target).value;
+
+  }
 }
+
+
+// HTMLInputElement
